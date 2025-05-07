dfac5872142891e227da409be50d4721f8b69b0c
adrirf7 <arodriguez20042008@gmail.com>
2025-05-07 11:40:56 +0200

    Hito 2 Parte 2 Programacion

--- Programacion_H2_Parte1_3T_AdrianRodriguezFernandez/src/GestionCine.java
+++ Programacion_H2_Parte1_3T_AdrianRodriguezFernandez/src/GestionCine.java
@@ -1,4 +1,3 @@
-import java.sql.*;
 import java.util.Scanner;
 public class GestionCine {
 	public static void main(String[] args) {
@@ -11,10 +10,13 @@ public class GestionCine {
 	    int opcion =0; // valor para el switch
 	    
 	    //Menu de opciones
-	    while(opcion !=2) {
+	    while(opcion !=5) {
 	    	System.out.println("\n-------Cine App-------");
 			System.out.println("1. Mostrar Peliculas");
-			System.out.println("2.Salir");
+			System.out.println("2.Añadir Peliculas");
+			System.out.println("3.Eliminar Peliculas");
+			System.out.println("4.Modificar Peliculas");
+			System.out.println("5.Salir");
 			System.out.print("Ingresa una opcion: ");
 			opcion = scanner.nextInt();
 			scanner.nextLine();
@@ -23,11 +25,24 @@ public class GestionCine {
 			
 			//Mostrar todas las peliculas
 			case 1:
-				verPeliculas(url, usuario, contraseña);
+				PeliculasCrud.verPeliculas(url, usuario, contraseña);
 				break;
 			
-			//Salir del programa
+			//Insertar una nueva pelicula
 			case 2: 
+				PeliculasCrud.insertarPelicula(url, usuario, contraseña);
+				break;
+			
+			//Eliminar Pelicula
+			case 3:
+				PeliculasCrud.eliminarPelicula(url, usuario, contraseña);
+				break;
+			
+			case 4:
+				PeliculasCrud.modificarPelicula(url, usuario, contraseña);
+				break;
+			//Salir del programa
+			case 5: 
 				System.out.println("Saliendo del programa...");
 				break;
 			
@@ -36,36 +51,5 @@ public class GestionCine {
 			}
 	    }
 	    scanner.close();
-	}
-	    public static void verPeliculas(String url, String usuario, String contraseña) {
-	    	try {
-	    		Connection conexion = DriverManager.getConnection(url, usuario, contraseña);
-	    		Statement stmt = conexion.createStatement();
-	    		
-	    		//Se establece la consulta a la BD
-	    		String consulta = "SELECT peliculas.id_pelicula, peliculas.titulo, peliculas.director, "
-	    				+ "peliculas.duracion, peliculas.fecha_estreno, generos.nombre AS genero "
-	    				+ "FROM peliculas JOIN generos ON peliculas.id_genero = generos.id_genero";
-	    		
-	    		ResultSet rs = stmt.executeQuery(consulta); //Ejecutamos la consulta
-	    		
-	    		//Se muestra la informacion recogida de forma ordenada
-	    		System.out.println("\n --------Listado de peliculas-----------");
-	    		while (rs.next()) {
-	    			System.out.println("------------------------------");
-	                System.out.println("ID: " + rs.getString("id_pelicula")); // id_pelicula
-	                System.out.println("Título: " + rs.getString("titulo")); // Titulo
-	                System.out.println("Director: " + rs.getString("director")); // Director
-	                System.out.println("Duración: " + rs.getInt("duracion") + " minutos"); // Duracion(minutos)
-	                System.out.println("Fecha de Estreno: " + rs.getDate("fecha_estreno")); // Fecha de esteno
-	                System.out.println("Género: " + rs.getString("genero")); //Genero
-	    		}
-	    		
-	    		rs.close();
-	    		stmt.close();
-	    		conexion.close();
-	    	} catch(SQLException e) {
-	    		System.out.println("Error: " + e.getMessage());
-	    	}
-	    }
+		}
 	}
